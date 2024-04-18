837a0fc591d0df23d5f621b729237ce5cc6f75f0
AkhileshManda <manda.akhilesh4@gmail.com>
2024-04-18 17:01:53 +0530

    chore : used gnois apis instead of moralis

--- src/hooks/useMoralis.tsx
+++ src/hooks/useMoralis.tsx
@@ -6,35 +6,53 @@ import { EvmChain, EvmTransaction } from "@moralisweb3/common-evm-utils";
 export const useMoralis = () => {
   const { address } = useAccount();
 
-  const [transaction, setTransactions] = useState<EvmTransaction[]>([]);
+  const [transaction, setTransactions] = useState<any>([]);
   const [moralisInit, setMoralisInit] = useState<boolean>(false);
 
-  const initMoralis = async () => {
-    if (!moralisInit) {
-      await Moralis.start({
-        apiKey: process.env.NEXT_PUBLIC_MORALIS_API_KEY,
-        defaultEvmApiChain: EvmChain.SEPOLIA,
-      });
-      setMoralisInit(true);
+  // const initMoralis = async () => {
+  //   if (!moralisInit) {
+  //     await Moralis.start({
+  //       apiKey: process.env.NEXT_PUBLIC_MORALIS_API_KEY,
+  //       defaultEvmApiChain: EvmChain.SEPOLIA,
+  //     });
+  //     setMoralisInit(true);
+  //   }
+  // };
+
+  const getUserTransactionsGnosis = async () => {
+    try {
+      const response = await fetch(
+        `https://api.gnosisscan.io/api?module=account&action=txlist&address=${address}&startblock=1&endblock=99999999&sort=asc&apikey=${process.env.NEXT_GNOSIS_API_KEY}`
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      const data = await response.json();
+      console.log(data.result);
+      setTransactions(data.result);
+      return data;
+    } catch (error) {
+      console.error(error);
     }
   };
 
-  const getUserTransactions = async () => {
-    await initMoralis();
-    const response = await Moralis.EvmApi.transaction.getWalletTransactions({
-      address: address!,
-    });
+  // const getUserTransactions = async () => {
+  //   await initMoralis();
+  //   const response = await Moralis.EvmApi.transaction.getWalletTransactions({
+  //     address: address!,
+  //   });
 
-    console.log(response.toJSON());
+  //   // console.log(response.toJSON());
 
-    setTransactions(response.result);
+  //   setTransactions(response.result);
 
-    return response;
-  };
+  //   return response;
+  // };
 
   useEffect(() => {
-    getUserTransactions();
+    // getUserTransactions();
+    getUserTransactionsGnosis();
   }, []);
 
-  return { getUserTransactions, transaction };
+  return { getUserTransactionsGnosis, transaction };
 };
