e20890834577546d65d15c486a678b798581146b
kalashshah <202051096@iiitvadodara.ac.in>
2024-04-21 20:50:45 +0530

    chore: update head section

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,13 @@ import Moralis from "moralis";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+        <title>Gnosis Lounge</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
